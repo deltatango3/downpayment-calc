214b185a933dc6635f0edccf598620bf5b8d4f8f
David Tran <davidsavtran@gmail.com>
2019-05-19 14:49:14 -0400

    downpayment calc functionality

---
+++ src/components/Result/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Result from './Result';
+
+const ResultContainer = props => {
+  return <Result downpayment={props.downpayment} />;
+};
+
+export default ResultContainer;
